c99b16b1c59aee461628525fccbdf0bdde6ccb39
YaoTing <94820616+YaoTingYao@users.noreply.github.com>
2024-04-17 07:03:15 -0400

    Uploaded app initial script

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Create app
+def create_app():
+    app=Flask(__name__)
+    return app
+
+app=create_app()
+
+# Create route
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(host="0,0,0,0", debug=True, port=5000, threaded=True)
